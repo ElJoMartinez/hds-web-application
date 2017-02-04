03e9f65ce5a5580b6d3b2065f55b2c087bee2f9a
Elliot Martinez <toillezenitram@gmail.com>
2017-02-04 16:22:23 -0500

    added user controller java class

---
+++ src/java/home/controllers/UserController.java
@@ -0,0 +1,102 @@
+package home.controllers;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+import home.business.User;
+import home.data.UserDB;
+
+public class UserController extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request,
+            HttpServletResponse response)
+            throws IOException, ServletException {
+
+        String requestURI = request.getRequestURI();
+        String url = "";
+        if (requestURI.endsWith("/deleteCookies")) {
+            url = deleteCookies(request, response);
+            getServletContext()
+                .getRequestDispatcher(url)
+                .forward(request, response);
+        } else if (requestURI.endsWith("/logout")) {
+            logout(request, response);
+        }
+        
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request,
+            HttpServletResponse response)
+            throws IOException, ServletException {
+
+        String requestURI = request.getRequestURI();
+        String url = "";
+        if (requestURI.endsWith("/subscribeToEmail")) {
+            url = subscribeToEmail(request, response);
+        }
+        getServletContext()
+                .getRequestDispatcher(url)
+                .forward(request, response);
+    }
+
+    private String deleteCookies(HttpServletRequest request,
+            HttpServletResponse response) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            cookie.setMaxAge(0);  //delete the cookie
+            cookie.setPath("/");  //entire application
+            response.addCookie(cookie);
+        }
+        return "/delete_cookies.jsp";
+    }
+
+    private String subscribeToEmail(HttpServletRequest request,
+            HttpServletResponse response) {
+
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String email = request.getParameter("email");
+
+        User user = new User();
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setEmail(email);
+
+        request.setAttribute("user", user);
+
+        String url;
+        String message;
+        //check that email address doesn't already exist
+        if (UserDB.emailExists(email)) {
+            message = "This email address already exists. <br>"
+                    + "Please enter another email address.";
+            request.setAttribute("message", message);
+            url = "/email/index.jsp";
+        } else {
+            UserDB.insert(user);
+            message = "";
+            request.setAttribute("message", message);
+            url = "/email/thanks.jsp";
+        }
+        return url;
+    }
+    
+    private void logout(HttpServletRequest request,
+            HttpServletResponse response) {
+        HttpSession session = request.getSession();
+        session.invalidate();
+        
+        String relativeURL = request.getContextPath();
+
+        try {
+            response.sendRedirect(relativeURL + "/login.jsp");
+        } catch (IOException ex) {
+            Logger.getLogger(UserController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}

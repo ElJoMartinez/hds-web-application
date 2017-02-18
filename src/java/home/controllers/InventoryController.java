d6cc9a7d2ce4763dfa9831a4907577c401574e24
Elliot Martinez <toillezenitram@gmail.com>
2017-02-17 19:05:50 -0500

    added inventory controller class to handle request responses from jsp

---
+++ src/java/home/controllers/InventoryController.java
@@ -0,0 +1,85 @@
+
+package home.controllers;
+
+/**
+ *
+ * @author Elliot Martinez
+ * Change log: 2/17/17 - added basic servlet skeleton
+ */
+
+import java.io.File;
+import java.io.IOException;
+
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+import home.business.*;
+import home.data.*;
+import home.util.CookieUtil;
+
+
+public class InventoryController extends HttpServlet {
+    
+    @Override
+    public void doGet(HttpServletRequest request, 
+            HttpServletResponse response) throws ServletException, IOException {
+        String requestURI = request.getRequestURI();
+        String url;
+        if (requestURI.endsWith("/listen")) {
+            url = listen(request, response);
+        }else if (requestURI.endsWith("/showinventory")) {
+            url = showInventory(request, response);
+        }else {
+            url = showProduct(request, response);
+        }
+        getServletContext()
+                .getRequestDispatcher(url)
+                .forward(request, response);
+            
+    }
+    
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String requestURI = request.getRequestURI();
+        String url = "/inventory";
+        if (requestURI.endsWith("/register")) {
+            url = registerUser(request, response);
+        }
+        getServletContext()
+                .getRequestDispatcher(url)
+                .forward(request, response);
+    }
+    
+    private String registerUser(HttpServletRequest request,
+            HttpServletResponse response) {
+
+        HttpSession session = request.getSession();
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String email = request.getParameter("email");
+
+        User user = new User();
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setEmail(email);
+        if (UserDB.emailExists(email)) {
+            UserDB.update(user);
+        } else {
+            UserDB.insert(user);
+        }
+
+        session.setAttribute("user", user);
+
+        Cookie emailCookie = new Cookie("emailCookie", email);
+        emailCookie.setMaxAge(60 * 60 * 24 * 365 * 2);
+        emailCookie.setPath("/");
+        response.addCookie(emailCookie);
+        
+        Product product = (Product) session.getAttribute("product");
+        String url = "/catalog/" + product.getSerialId() + "/sound.jsp";
+        return url;
+    }
+    
+}
